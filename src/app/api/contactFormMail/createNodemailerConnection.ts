21aae13e890e49f2a4129189650acfdda6a46427
Thamiris Vicente <thamiavicente@gmail.com>
2025-06-24 20:46:49 +0200

    Add nodemailer connection

---
+++ src/app/api/contactFormMail/createNodemailerConnection.ts
@@ -0,0 +1,54 @@
+const nodemailer = require("nodemailer");
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GOOGLE_EMAIL,
+    pass: process.env.GOOGLE_APP_PASSWORD,
+  },
+});
+
+export const sendEmail = async (name: string, email: string, telephone: string, message: string) => {
+  try {
+    const info = await transporter.sendMail({
+      from: 'ThamiAVicente - Vercel',
+      to: process.env.GOOGLE_EMAIL,
+      subject: `From: ${name}`,
+      html:
+      `${message}
+      <hr>
+      <b>from:</b> ${name}
+      <br>
+      <b>telephone:</b> ${telephone}
+      <br>
+      <b>email:</b> ${email}`,
+    })
+
+    return {
+      status: 200,
+      statusMessage: 'Message sent successfully'
+    }
+
+  } catch (err) {
+    return {
+      error: err,
+      status: 500,
+      statusMessage: 'Could not send the message'
+    }
+  }
+}
+
+export const testConnection = async () => {
+  const connection = await transporter.verify()
+  const response = {
+    status: 500,
+    statusMessage: 'Could not connect to e-mail'
+  }
+
+  if(connection) {
+    response. status = 200
+    response.statusMessage = 'Connected with success'
+  }
+
+  return response
+}
